d3070a0acbf398734dc8f9b4ab3d92339c1d8546
Brad <31802085+bnrobinson93@users.noreply.github.com>
2026-09-17 23:12:02 -0500

    ch10

---
+++ ch10/exerciseOne/exerciseOne.go
@@ -0,0 +1,6 @@
+// Package exerciseone exposes a single function named Add
+package exerciseone
+
+func Add(a, b int) int {
+	return a + b
+}
---
+++ ch10/exerciseOne/go.mod
@@ -0,0 +1,3 @@
+module github.com/bnrobinson93/learning-go/ch10/exerciseOne
+
+go 1.27.1
