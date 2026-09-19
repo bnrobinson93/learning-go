0c22ebed9bae13ff39912139bd87d80e1a60ac8c
Brad <31802085+bnrobinson93@users.noreply.github.com>
2026-09-18 23:01:02 -0500

    godoc

--- ch10/exerciseOne/exerciseOne.go
+++ ch10/exerciseOne/exerciseOne.go
@@ -1,6 +1,11 @@
 // Package exerciseone exposes a single function named Add
 package exerciseone
 
+// Add adds two integers and returns the result
+//
+// For more, check out this link about [addition].
+//
+// [addition]: https://mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
